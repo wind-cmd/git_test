3a5375b0c0f6b151985b419749cf4ce7b1a95833
wind-cmd <1262507681@qq.com>
2025-06-14 20:57:49 +0800

    添加员工以及工作经历事务管理，新添加员工添加日志数据库，可进行记录

--- 06_springboot-demo/src/main/java/com/example/service/impl/EmpServiceImpl.java
+++ 06_springboot-demo/src/main/java/com/example/service/impl/EmpServiceImpl.java
@@ -8,13 +8,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.mapper.EmpExprMapper;
 import com.example.mapper.EmpMapper;
 import com.example.pojo.Emp;
 import com.example.pojo.EmpExpr;
+import com.example.pojo.EmpLog;
 import com.example.pojo.EmpQueryParam;
 import com.example.pojo.PageResult;
+import com.example.service.EmpLogService;
 import com.example.service.EmpService;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
@@ -24,12 +27,14 @@ public class EmpServiceImpl implements EmpService {
 
     private final EmpMapper empMapper;
     private final EmpExprMapper empExprMapper;
+    private final EmpLogService empLogService;
     private static final Logger logger = LoggerFactory.getLogger(EmpServiceImpl.class);
 
     @Autowired
-    public EmpServiceImpl(EmpMapper empMapper, EmpExprMapper empExprMapper) {
+    public EmpServiceImpl(EmpMapper empMapper, EmpExprMapper empExprMapper, EmpLogService empLogService) {
         this.empMapper = empMapper;
         this.empExprMapper = empExprMapper;
+        this.empLogService = empLogService;
     }
 
     // public PageResult<Emp> page(Integer page, Integer pageSize, String name,
@@ -55,23 +60,37 @@ public class EmpServiceImpl implements EmpService {
         return pageResult;
     }
 
+    /**
+     * 设置事务，只要出现错误就会滚，不管是不是运行时异常。
+     * 事务注解写在方法上，方法内操作要么全部执行，要么回滚。
+     */
+    @Transactional(rollbackFor = { Exception.class })
     @Override
     public void save(Emp emp) {
-        // 设置时间员工创建时间，修改时间
-        emp.setCreateTime(LocalDateTime.now());
-        emp.setUpdateTime(LocalDateTime.now());
-        // 保存员工
-        empMapper.add(emp);
-        // 获取工作经历
-        List<EmpExpr> empExprs = emp.getExprList();
-        if (!empExprs.isEmpty()) {
-            // 遍历工作经历，
-            empExprs.forEach(empExpr -> {
-                // 赋值empId，知道是哪个员工的经历
-                empExpr.setEmpId(emp.getId());
-            });
+
+        try { // 1.存员工基本信息
+              // 设置时间员工创建时间，修改时间
+            emp.setCreateTime(LocalDateTime.now());
+            emp.setUpdateTime(LocalDateTime.now());
+            empMapper.add(emp);
+
+            // 获取工作经历
+            List<EmpExpr> empExprs = emp.getExprList();
+            if (!empExprs.isEmpty()) {
+                // 遍历工作经历，
+                empExprs.forEach(empExpr -> {
+                    // 赋值empId，知道是哪个员工的经历
+                    empExpr.setEmpId(emp.getId());
+                });
+                // 2.保存员工经历
+                empExprMapper.add(emp.getExprList());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 3.记录操作日志
+            EmpLog empLog = new EmpLog(null, LocalDateTime.now(), "新增员工：" + emp);
+            empLogService.insertLog(empLog);
         }
-        // 保存员工经历
-        empExprMapper.add(emp.getExprList());
     }
 }
